8afa6a4164c22afb9a1a6491330bb74f7ceaeb99
Precession <precession.cv01@gmail.com>
2022-05-10 19:31:01 +0900

    Modyfied for exec by shell script

--- 01_image2csv/src/ConvertImg2csv.py
+++ 01_image2csv/src/ConvertImg2csv.py
@@ -1,24 +1,23 @@
 import csv
 import numpy
 import os
+import sys
 
 import cv2    # OpenCV
 
-ext = "jpg"
-img_name = "test"
-csv_name = "test"
+if __name__ == '__main__':
+    args = sys.argv
+    if 2 <= len(args):
+        # 引数から対象画像のパスを取得
+        img_path = os.path.join('../input/', args[1])
 
-input_path = r'../input'
-output_path = r'../output'
+        bgr_array = cv2.imread(img_path)
 
-fname = img_name + "." + ext
-fpath = os.path.join(input_path, fname)
-bgr_array = cv2.imread(fpath)
-
-fnamelist = ["_b", "_g", "_r"]
-for bgr in range(3):
-    colorArray = numpy.array(bgr_array[:,:,bgr])
-    csvname = csv_name + fnamelist[bgr] + ".csv"
-    csvpath = os.path.join(output_path, csvname)
-    numpy.savetxt(csvpath, colorArray, fmt = "%.0f", delimiter = ",")
-
+        fnamelist = ["_b", "_g", "_r"]
+        for bgr in range(3):
+            colorArray = numpy.array(bgr_array[:,:,bgr])
+            csvname = os.path.splitext(os.path.basename(args[1]))[0] + fnamelist[bgr] + ".csv"
+            csvpath = os.path.join('../output/', csvname)
+            numpy.savetxt(csvpath, colorArray, fmt = "%.0f", delimiter = ",")
+    else:
+        print('Arguments are too short')
